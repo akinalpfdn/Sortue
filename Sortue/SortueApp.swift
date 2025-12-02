eb2f6a0898b3d47b1e5ccbf74ce4da6e566cb950
Akinalp Fidan <81157056+akinalpfdn@users.noreply.github.com>
2025-12-02 22:50:58 +0300

    Initial Commit

---
+++ Sortue/SortueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortueApp.swift
+//  Sortue
+//
+//  Created by Akinalp Fidan on 2.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SortueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
